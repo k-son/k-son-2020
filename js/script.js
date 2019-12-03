669e469412f2110f1727ba05c86d283fbd5263db
Jacek Jarczok <kson.eu@gmail.com>
2019-12-03 09:13:47 +0100

    Scroll dwon btn value

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+"use strict";
 
 const scrollDown = document.querySelector('.header__scroll-down');
 
 function hideScrollDownBtn() {
-  scrollDown.style.display = 'none';
+  if ((document.body.scrollTop > 50) || (document.documentElement.scrollTop > 50)) {
+    scrollDown.style.display = 'none';
+  }
 }
 window.onscroll = hideScrollDownBtn;
