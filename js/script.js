48955527c5b9336b652ca757038d3ba6581abe5c
Jacek Jarczok <kson.eu@gmail.com>
2019-12-04 07:35:34 +0100

    Develop fade effect

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 
 // Header fade on scroll down
 const header = document.querySelector('.header');
+const headerCaption = document.querySelector('.header__caption__h1');
+const headerCaptionShadow = document.querySelector('.header__caption__shadow');
 let viewportHeight;
 let num;
 let heightBreakPoints = [];
@@ -12,8 +14,8 @@ function getViewportHeight() {
 getViewportHeight();
 window.addEventListener('resize', getViewportHeight);
 
-num = viewportHeight / 15; // increase the number after division sing if you want the header to fade sooner
-for (let i=1; i<=10 ;i++) {
+num = viewportHeight / 100; // increase the number after division sing if you want the header to fade sooner
+for (let i=1; i<=100 ;i++) {
   heightBreakPoints.push(num*i);
 }
 
@@ -22,59 +24,38 @@ function fadeHeader() {
 
   if (yOffset > heightBreakPoints[0] && yOffset <heightBreakPoints[1]) {
     header.style.opacity = .8;
+    header.style.height = "90vh";
   } else if (yOffset >= heightBreakPoints[1] && yOffset < heightBreakPoints[2]) {
     header.style.opacity = .7;
+    header.style.height = "85vh";
   } else if (yOffset >= heightBreakPoints[2] && yOffset < heightBreakPoints[3]) {
     header.style.opacity = .6;
+    header.style.height = "80vh";
   }  else if (yOffset >= heightBreakPoints[3] && yOffset < heightBreakPoints[4]) {
     header.style.opacity = .3;
+    header.style.height = "75vh";
   }  else if (yOffset >= heightBreakPoints[4] && yOffset < heightBreakPoints[5]) {
     header.style.opacity = .2;
+    header.style.height = "70vh";
   }  else if (yOffset >= heightBreakPoints[5] && yOffset < heightBreakPoints[6]) {
     header.style.opacity = .1;
+    header.style.height = "65vh";
   } else if (yOffset >= heightBreakPoints[6]) {
     header.style.opacity = 0;
+    header.style.height = "60vh";
   } else {
     header.style.opacity = 1;
+    header.style.height = "100vh";
   }
 }
 
 window.addEventListener('scroll', fadeHeader);
 
-/*
-function fadeHeader() {
-  let yOffset = window.pageYOffset;
-
-  if (yOffset > 50 && yOffset < 100) {
-    header.style.opacity = .8;
-  } else if (yOffset >= 100 && yOffset < 150) {
-    header.style.opacity = .7;
-  } else if (yOffset >= 150 && yOffset < 200) {
-    header.style.opacity = .6;
-  }  else if (yOffset >= 200 && yOffset < 250) {
-    header.style.opacity = .5;
-  }  else if (yOffset >= 250 && yOffset < 300) {
-    header.style.opacity = .4;
-  }  else if (yOffset >= 300 && yOffset < 350) {
-    header.style.opacity = .3;
-  }  else if (yOffset >= 350 && yOffset < 400) {
-    header.style.opacity = .2;
-  }  else if (yOffset >= 400 && yOffset < 450) {
-    header.style.opacity = .1;
-  }  else if (yOffset >= 450) {
-    header.style.opacity = 0;
-  } else {
-    header.style.opacity = 1;
-  }
-}
-*/
-
-
 // scroll dwon button
 const scrollDown = document.querySelector('.header__scroll-down');
 
 function hideScrollDownBtn() {
-  if ((document.body.scrollTop > 50) || (document.documentElement.scrollTop > 50)) {
+  if ((document.body.scrollTop > 30) || (document.documentElement.scrollTop > 50)) {
     scrollDown.style.display = 'none';
   } else {
     scrollDown.style.display = 'block';
@@ -85,4 +66,3 @@ window.onscroll = hideScrollDownBtn;
 scrollDown.addEventListener('click', () => {
   document.querySelector('.navigation').scrollIntoView();
 })
-
