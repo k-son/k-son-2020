8a371e155522cf986965638306be04e7964569d8
Jacek Jarczok <kson.eu@gmail.com>
2021-02-24 17:33:07 +0100

    Develop contact form

--- send-script.php
+++ send-script.php
@@ -1,26 +1,58 @@
 <?php
+
 $mailToSend = "kson.eu@gmail.com";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $message = $_POST["email"];
+    $phone = $_POST["phone"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
     $antiSpam = $_POST["honey"];
 
     $errors = Array();
 	  $return = Array();
 
+    if (empty($name)) { //jeżeli pusta wartość
+        array_push($errors, "name");
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //sprawdzamy czy email ma zły wzór
+        array_push($errors, "email");
+    }
+    if (empty($message)) {
+        array_push($errors, "message");
+    }
+
     if (empty($antiSpam)) {
+      if (count($errors) > 0) {
+          $return["errors"] = $errors;
+      } else {
+          //każde wysłanie wiadomości musi być poprzedzone ustawieniem nagłówków
+          $headers  = "MIME-Version: 1.0" . "\r\n";
+          $headers .= "Content-type: text/html; charset=UTF-8". "\r\n";
+          $headers .= "From: ".$email."\r\n";
+          $headers .= "Reply-to: ".$email;
+          $message  = "
+              <html>
+                  <head>
+                      <meta charset=\"utf-8\">
+                  </head>
+                  <body>
+                      <div> Imię: $name</div>
+                      <div> Email: <a href=\"mailto:$email\">$email</a> </div>
+                      <div> Telefon: $phone</div>
+                      <div> Temat: $subject</div>
+                      <div> Wiadomość: </div>
+                      <div> $message </div>
+                  </body>
+              </html>";
 
-        //tutaj cała reszta skryptu który mieliśmy wcześniej (od 23 do 49)
-        if (count($errors) > 0) {
-            $return["errors"] = $errors;
-        } else {
-            ...
-        }
-
-    } else {
-        $return["status"] = "ok";
+          if (mail($mailToSend, "Wiadomość ze strony - " . date("d-m-Y"), $message, $headers)) {
+              $return["status"] = "ok";
+          } else {
+              $return["status"] = "error";
+          }
+      }
     }
 
     header("Content-Type: application/json");
